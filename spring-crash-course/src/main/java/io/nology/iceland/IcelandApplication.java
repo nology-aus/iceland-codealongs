d35705f62ee3fd210ac77b3b036af11652b327af
Calum Hill-Cawthorne <chillcaw@gmail.com>
2021-11-09 10:57:33 +1100

    spring crash course pushed

---
+++ spring-crash-course/src/main/java/io/nology/iceland/IcelandApplication.java
@@ -0,0 +1,13 @@
+package io.nology.iceland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcelandApplication.class, args);
+	}
+
+}
---
+++ spring-crash-course/src/test/java/io/nology/iceland/IcelandApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.iceland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcelandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
